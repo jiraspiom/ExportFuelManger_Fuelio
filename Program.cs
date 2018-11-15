cbf62a8206a1a56421643c1f8b90757de4225566
jiraspiom <jiraspiom@yahoo.com.br>
2018-11-15 18:29:12 -0200

    inicial

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Globalization;
+using exportFuel.entidade;
+using System.Text.RegularExpressions;
+
+namespace exportFuel
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+     
+            gasolinha.programa();
+
+        }
+
+
+    }
+}
